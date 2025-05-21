cea6bf45acc20248d289799096f9f5da51925178
Izunamori <158246362+Izunamori@users.noreply.github.com>
2025-05-21 13:33:05 +0500

    Update autostart.sh

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
+
+### Монитор ### -----------------------------------------------------------------------
 xrandr --output DP-0 --mode 2560x1440 --rate 164.83 &
-/./home/vaniels/deepcool-digital-linux &
+
+
+### Раскладки ### ---------------------------------------------------------------------
 setxkbmap -layout us,ru -variant -option grp:alt_shift_toggle,terminate:ctrl_alt_cksp &
 lxsession &
 
+
+### прочее ### ------------------------------------------------------------------------
 telegram-desktop &
 clipit &
 xcompmgr &
-# picom --config $HOME/.config/picom/1.conf &
 # steam -silent &
 # discord --start-minimized &
 xset m 1 1 &
