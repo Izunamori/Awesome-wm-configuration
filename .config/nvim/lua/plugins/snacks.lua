f167d9477ed78d60d02714b5fc42def8b75c70f7
Izunamori <q@izunamori.ru>
2026-03-01 14:02:02 +0500

    update configuration

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,24 @@
+return {
+  "snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        pick = function(cmd, opts)
+          return LazyVim.pick(cmd, opts)()
+        end,
+        header = [[Neovim]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Neovim config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "a", desc = "AwesomeWM config", action = function() LazyVim.pick("files", { cwd = vim.fn.expand("~/.config/awesome") })() end, },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+        },
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/vscode.lua
@@ -0,0 +1,12 @@
+return {
+"Mofiqul/vscode.nvim",
+config = function()
+  require("vscode").setup({
+    style = "dark",         
+    transparent = true,    
+    italic_comments = true, 
+    disable_nvimtree_bg = true,
+  })
+  vim.cmd([[colorscheme vscode]])
+end,
+}
