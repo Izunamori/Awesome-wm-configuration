80d5a2d7a6da348a96a66849b4fa718246ca1b73
Izunamori <q@izunamori.ru>
2026-04-26 00:21:28 +0500

    update configuration

--- modules/binds.lua
+++ modules/binds.lua
@@ -31,9 +31,7 @@ local active_keys = {}
 
 --- Mouse binds ---
 root.buttons(gears.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    awful.button({ }, 3, function () mymainmenu:toggle() end)
 ))
 
 --- Key binds ---
