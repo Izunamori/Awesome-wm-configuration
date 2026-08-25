26bfa0f6141e74fe20a60b1c179b2da4356b7a13
Izunamori <q@izunamori.ru>
2026-08-25 20:54:16 +0500

    update configuration

--- scripts/functional/gamma.sh
+++ scripts/functional/gamma.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 OUTPUT="DP-2"
+
 current=$(xrandr --verbose | grep -A 10 "$OUTPUT" | grep "Gamma:" | awk '{print $2}')
 
-if [[ "$current" == *"1:1:1"* ]] || [[ "$current" == *"1.0:1.0:1.0"* ]]; then
-    xrandr --output "$OUTPUT" --gamma 2:2:2
+current_val=$(echo "$current" | cut -d':' -f1)
+
+if [[ "$current_val" == "1.0" ]]; then
+    new_gamma="1.5:1.5:1.5"
+elif [[ "$current_val" == "0.67" ]]; then
+    new_gamma="2.0:2.0:2.0"
+elif [[ "$current_val" == "0.5" ]]; then
+    new_gamma="1.0:1.0:1.0"
 else
-    xrandr --output "$OUTPUT" --gamma 1:1:1
+    new_gamma="1.0:1.0:1.0"
 fi
+
+xrandr --output "$OUTPUT" --gamma "$new_gamma"
+
+echo "Gamma changed to $new_gamma (xrandr shows: $(xrandr --verbose | grep -A 10 "$OUTPUT" | grep "Gamma:" | awk '{print $2}'))"
